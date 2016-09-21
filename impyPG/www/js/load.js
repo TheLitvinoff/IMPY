633fe40fc5544605068bd47d9f2b521023a6468b
marina <arthurzarovich@gmail.com>
2016-09-21 16:01:56 +0700

    added images to load state

--- impyPG/www/js/load.js
+++ impyPG/www/js/load.js
@@ -1,6 +1,14 @@
 var loadState = {
 	preload: function() {
-		
+		var loadingLabel = game.add.text(game.width/2, 150, 'loading...', {font: '30px Arial', fill: '#ffffff'});
+		loadingLabel.anchor.setTo(0.5, 0.5);
+
+		game.load.spritesheet('player', '', 0, 0);
+		game.load.image('background', 'assets/background.png');
+		game.load.image('clean', 'assets/cleanIcon.png');
+		game.load.image('food', 'assets/foodIcon.png');
+		game.load.image('play', 'assets/playIcon.png');
+		game.load.image('goOut', 'assets/GoOutIcon.png');
 	},
 
 	create: function() {
