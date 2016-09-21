e7507873fe0e21393435c14348b791b761277b47
marina <arthurzarovich@gmail.com>
2016-09-21 16:52:12 +0700

    fngjdfgj

--- impyPG/platforms/android/assets/www/js/game.js
+++ impyPG/platforms/android/assets/www/js/game.js
@@ -1,9 +1,5 @@
 var game = new Phaser.Game(768, 1280, Phaser.AUTO, 'deviceready');
 
-game.global = {
-	
-};
-
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('playNameChar', playNameCharState);
