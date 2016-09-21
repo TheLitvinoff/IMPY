72b627cc69721ac16e293de0c4861d77a38b53bb
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-09-21 15:53:02 +0700

    Add physics and renderer

--- impyPG/www/js/boot.js
+++ impyPG/www/js/boot.js
@@ -3,6 +3,10 @@ var bootState = {
 	},
 
 	create: function() {
+		game.stage.backgroundColor = "#3498db";
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.renderer.renderSession.roundPixels = true;
+		
 		game.state.start('load');
 	},
 }
