c4cb6eb7c9db9c47c395cd60ef7f82d98d08fdae
marina <arthurzarovich@gmail.com>
2016-09-21 15:32:51 +0700

    basic state config

--- impyPG/www/js/game.js
+++ impyPG/www/js/game.js
@@ -1,12 +1,12 @@
-var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(500, 340, Phaser.AUTO, 'deviceready');
 
 game.global = {
-	score: 0
+	
 };
 
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
-game.state.add('menu', menuState);
-game.state.add('play', playState);
+game.state.add('playNameChar', playNameCharState);
+game.state.add('playMain', playMainState);
 
 game.state.start('boot');
--- impyPG/www/js/load.js
+++ impyPG/www/js/load.js
@@ -1,37 +1,9 @@
 var loadState = {
 	preload: function() {
-		//label
-		var loadingLevel = game.add.text(game.width/2, 150, 'Loading...', {
-			font: '30px, Arial',
-			fill: '#ffffff'
-		});
-		loadingLevel.anchor.setTo(0.5,0.5);
-
-		//progress bar
-		var progressBar = game.add.sprite(game.width/2, 200, 'progressBar')
-		progressBar.anchor.setTo(0.5,0.5);
-		game.load.setPreloadSprite(progressBar);
-
-		//load all the assets
-		//game.load.image('player', 'assets/player.png');
-		game.load.image('wallV', 'assets/wallVertical.png');
-		game.load.image('wallH', 'assets/wallHorizontal.png');
-		game.load.image('coin', 'assets/coin.png');
-		game.load.image('enemy', 'assets/enemy.png');
-		game.load.image('pixel', 'assets/pixel.png');
-
-		game.load.image('background', 'assets/background.png');
-
-		game.load.audio('jump', ['assets/jump.ogg', 'assets/jump.mp3']);
-		game.load.audio('coin', ['assets/coin.ogg', 'assets/coin.mp3']);
-		game.load.audio('dead', ['assets/dead.ogg', 'assets/dead.mp3']);
-
-		//spritesheets
-		game.load.spritesheet('player', 'assets/player2.png', 20, 20);
-		game.load.spritesheet('mute', 'assets/muteButton.png', 28,22);
+		
 	},
 
 	create: function() {
-		game.state.start('menu');
+		game.state.start('playNameChar');
 	},
 }
