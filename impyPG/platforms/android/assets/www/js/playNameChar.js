fd66121823127d86ec74420016a633c5c48b51e3
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-09-25 17:18:24 +0700

    Nothing important

--- impyPG/platforms/android/assets/www/js/playNameChar.js
+++ impyPG/platforms/android/assets/www/js/playNameChar.js
@@ -2,10 +2,8 @@ var playNameCharState = {
 	create: function(){
 		game.add.image(0,0,'background');
 
-		var nameLabel = game.add.text(game.width/2, -50, 'Impy: the little devil', {font: '50px Arial', fill: '#ffffff'});
+		var nameLabel = game.add.text(game.width/2, game.height/2, 'Impy: the little devil', {font: '50px Arial', fill: '#ffffff'});
 		nameLabel.anchor.setTo(0.5, 0.5);
-
-		
 
 	},
 
