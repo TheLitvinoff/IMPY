15906de4bb7804215154b250158fe6c49d388c58
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-09-26 12:03:05 +0700

    Input box added FINALLLLLYYYYYYY

--- impyPG/platforms/android/assets/www/js/playNameChar.js
+++ impyPG/platforms/android/assets/www/js/playNameChar.js
@@ -3,10 +3,23 @@ var playNameCharState = {
 		game.add.image(0,0,'background');
 		game.add.plugin(Fabrique.Plugins.InputField);
 
-		var nameLabel = game.add.text(game.width/2, game.height/2, 'Impy: the little devil', {font: '50px Arial', fill: '#ffffff'});
+		var nameLabel = game.add.text(game.width/2, game.height/4 - 80, 'Name your Impy:', {font: '50px Arial', fill: '#212121', fontWeight: 'bold'});
 		nameLabel.anchor.setTo(0.5, 0.5);
-		
-		var input = game.add.inputField(10, 90);
+
+		var input = game.add.inputField((game.width-500)/2, game.height/4, {
+		    font: '80px Arial',
+		    fill: '#212121',
+		    fontWeight: 'bold',
+		    width: 500,
+		    height: 100,
+		    padding: 8,
+		    borderWidth: 1,
+		    borderColor: '#000',
+		    borderRadius: 6,
+		    placeHolder: 'Type here...',
+		    padding: 10 
+		});
+
 
 	},
 
