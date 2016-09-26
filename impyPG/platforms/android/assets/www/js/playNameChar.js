937bb357b3ba710f271c32787e0e1c563c30ff05
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2016-09-26 11:08:00 +0700

    Input plugin

--- impyPG/platforms/android/assets/www/js/playNameChar.js
+++ impyPG/platforms/android/assets/www/js/playNameChar.js
@@ -1,9 +1,12 @@
 var playNameCharState = {
 	create: function(){
 		game.add.image(0,0,'background');
+		game.add.plugin(Fabrique.Plugins.InputField);
 
 		var nameLabel = game.add.text(game.width/2, game.height/2, 'Impy: the little devil', {font: '50px Arial', fill: '#ffffff'});
 		nameLabel.anchor.setTo(0.5, 0.5);
+		
+		var input = game.add.inputField(10, 90);
 
 	},
 
