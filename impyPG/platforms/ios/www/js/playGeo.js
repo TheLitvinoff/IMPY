73f7da06c2f6fab91f6fca79e3f0c9bafcfa369d
marina <arthurzarovich@gmail.com>
2016-09-28 14:05:09 +0700

    getGeo

--- impyPG/platforms/ios/www/js/playGeo.js
+++ impyPG/platforms/ios/www/js/playGeo.js
@@ -3,7 +3,7 @@ var playGeoState = {
 		this.goOutSprite = game.add.sprite(game.width - 100, 150, 'goOut');
 		this.goOutSprite.anchor.setTo(0.5, 0.5);
 		this.goOutSprite.inputEnabled = true;
-		this.goOutSprite.events.onInputDown.add(this.getGeo, this);
+		//this.goOutSprite.events.onInputDown.add(this.getGeo, this);
 
 		game.global.latLabel = game.add.text(game.width/2, game.height/4 - 80,  'latitude', {font: '50px Gloria Hallelujah', fill: '#ffffff', fontWeight: 'bold', align: 'center'});
 	   	game.global.latLabel.anchor.setTo(0.5, 0.5);
@@ -11,18 +11,16 @@ var playGeoState = {
 	    game.global.lonLabel.anchor.setTo(0.5, 0.5);
 	},
 	update: function() {
-
+		this.getGeo();
 	}, 
 	getGeo: function() {
-
-
 		var options = {
 	    	maximumAge: 3000, 
 	    	timeout: 5000, 
 	    	enableHighAccuracy: true
 	   	}
 		
-	   	var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+	   	var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
 	   	function onSuccess(position) {
 	   		var posLat = position.coords.latitude;
